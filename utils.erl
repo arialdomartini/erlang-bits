573d04b2ccf51c8d7f951fa5c2e3a03e112f79b2
Arialdo Martini <arialdomartini@gmail.com>
2013-08-06 09:07:28 +0200

    utils.count() counts from 1 to 10

--- utils.erl
+++ utils.erl
@@ -1,9 +1,13 @@
 -module(utils).
 -export([wc/1]).
 -export([wc/2]).
+-export([count/0]).
+-export([count/2]).
 
 wc(Words) -> wc(0, Words).
 wc(Len, []) -> Len;
 wc(Len, Words) -> wc(Len+1, erlang:tl(Words)).
 
-
+count() -> count(1,[1]).
+count(10, S)->S;
+count(L, S) -> count(L+1, lists:append(S, [erlang:length(S)+1])).
