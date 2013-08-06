13b11b3a3c6aad83b72cbc271c4d0a2e9dfd901f
Arialdo Martini <arialdomartini@gmail.com>
2013-08-06 18:54:54 +0200

    utils:positive uses if and guards to detect positive and negative numbers

--- utils.erl
+++ utils.erl
@@ -3,6 +3,7 @@
 -export([wc/2]).
 -export([count/0]).
 -export([count/2]).
+-export([positive/1]).
 
 wc(Words) -> wc(0, Words).
 wc(Len, []) -> Len;
@@ -11,3 +12,11 @@ wc(Len, Words) -> wc(Len+1, erlang:tl(Words)).
 count() -> count(1,[1]).
 count(10, S)->S;
 count(L, S) -> count(L+1, lists:append(S, [erlang:length(S)+1])).
+
+positive(N) ->
+  if 
+    N > 0 ->
+      "positive";
+    N < 0 ->
+      "negative"
+  end.
