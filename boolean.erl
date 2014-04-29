f104840553ff7e2fd907b4b94242119ec7b25fa8
Arialdo Martini <arialdomartini@gmail.com>
2014-04-29 18:31:50 +0200

    A boolean and

--- boolean.erl
+++ boolean.erl
@@ -1,5 +1,5 @@
 -module(boolean).
--export([b_not/1]).
+-export([b_not/1, b_and/2]).
 
 b_not(true) ->
     false;
@@ -7,3 +7,12 @@ b_not(false) ->
     true;
 b_not(_Object) ->
     {error, "object must be either true or false"}.
+
+b_and(false, _) ->
+    false;
+b_and(_, false) ->
+    false;
+b_and(true, true) ->
+    true;
+b_and(_,_) ->
+    {error, "arguments must be true or false"}.
