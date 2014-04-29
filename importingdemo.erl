3be1dec6a9964208d7385d0f614ae7c92a6a870a
Arialdo Martini <arialdomartini@gmail.com>
2014-04-29 08:42:44 +0200

    module_info() performs reflection on a module

--- importingdemo.erl
+++ importingdemo.erl
@@ -1,6 +1,9 @@
 -module(importingdemo).
 -import(demo, [double/1]).
--export([run/1]).
+-export([run/1, get_info_about_demo/0]).
 
 run(Value) ->
     double(Value).
+
+get_info_about_demo() ->
+  demo:module_info().
