eb79c79f085044e6f9d9de658a4e30d32e3df557
Arialdo Martini <arialdomartini@gmail.com>
2014-04-29 18:25:18 +0200

    Area calculation (with pattern matching)

---
+++ shapes.erl
@@ -0,0 +1,16 @@
+-module(shapes).
+-export([area/1]).
+
+area({square, Side}) ->
+    Side * Side;
+
+area({circle, Radius}) ->
+    math:pi() * Radius * Radius;
+
+area({triangle, A, B, C}) ->
+    S = ( A + B + C )/2,
+    math:sqrt(S*(S-A)*(S-B)*(S-C));
+
+area(Other) ->
+    {error, invalid_object}.
+
