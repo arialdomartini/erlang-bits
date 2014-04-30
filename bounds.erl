94a1f1c6193c69cebaec13a6824858b96e8f2f75
Arialdo Martini <arialdomartini@gmail.com>
2014-04-30 18:26:32 +0200

    safe scoping, with a variable single bounded

--- bounds.erl
+++ bounds.erl
@@ -1,9 +1,16 @@
 -module(bounds).
--export([safe/1]).
+-export([safe/1, better/1]).
 
 safe(X) ->
     case X of
         1 -> Y=12;
         _   -> Y= 196
     end,
+    X + Y.
+
+better(X) ->
+    Y = case X of
+            1 -> 12;
+            _ -> 196
+        end,
     X + Y.
