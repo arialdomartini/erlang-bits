0fe8c1db9aa7952ac5344115a64729eb967f88f3
Arialdo Martini <arialdomartini@gmail.com>
2014-04-30 08:43:39 +0200

    Calcutaes the length of a List using Tail recursion and a Case Expression

--- listhelpers.erl
+++ listhelpers.erl
@@ -1,7 +1,14 @@
 -module(listhelpers).
--export([listlen/1]).
+-export([listlen/1, listlen_case/1]).
 
 
-% Calculates the length of a list using tail recursion
+% Calculates the length of a list using tail recursion and pattern matching
 listlen([])         -> 0;
 listlen([_|Xs])     -> 1 + listlen(Xs).
+
+% Calculates the length of a list using tail recursion and a case expression
+listlen_case(Y) ->
+    case Y of
+        []          -> 0;
+        [_|Xs]      -> 1 + listlen_case(Xs)
+    end.
