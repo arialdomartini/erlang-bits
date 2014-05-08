f89c5dd66d8f31c503984d0d3f9d6efb7d696a1a
Arialdo Martini <arialdomartini@gmail.com>
2014-05-08 18:38:19 +0200

    Converting a string to an atom

--- to_string.erl
+++ to_string.erl
@@ -1,6 +1,9 @@
 -module(to_string).
--export([atom_to_string/1]).
+-export([atom_to_string/1, string_to_atom/1]).
 
 
 atom_to_string(Atom) ->
     atom_to_list(Atom).
+
+string_to_atom(String) ->
+    list_to_atom(String).
