03d8a543bbb1a565fc0e52951ef4389b0b655042
Arialdo Martini <arialdomartini@gmail.com>
2014-05-08 18:29:56 +0200

    Length of a list

--- bif.erl
+++ bif.erl
@@ -1,5 +1,5 @@
 -module(bif).
--export([head/0, tail/0]).
+-export([head/0, tail/0, length/0]).
 
 head() ->
     List = [one, two, three, four, five],
@@ -8,3 +8,7 @@ head() ->
 tail() ->
     List = [one, two, three, four, five],
     tl(List).
+
+length() ->
+    List = [one, two, three, found, five],
+    length(List).
