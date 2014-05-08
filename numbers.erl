d8145558a34ef1481ebb6fda0e74d51cc2e179fe
Arialdo Martini <arialdomartini@gmail.com>
2014-05-08 08:53:49 +0200

    A function using pattern matching, guards and a catch-all guard

--- numbers.erl
+++ numbers.erl
@@ -1,5 +1,9 @@
 -module(numbers).
--export([even/1]).
+-export([even/1, number/1]).
 
 even(Int) when Int rem 2 == 0 -> even;
 even(Int) when Int rem 2 == 1 -> odd.
+
+number(Num) when is_integer(Num)  -> integer;
+number(Num) when is_float(Num)    -> float;
+number(_Other)                    -> false.
