e061489ceede5876581ba880671cc19ccb9e8c3b
Arialdo Martini <arialdomartini@gmail.com>
2014-05-08 08:44:02 +0200

    Factorial using guards

--- factorial.erl
+++ factorial.erl
@@ -1,6 +1,10 @@
 -module(factorial).
--export([classic/1]).
+-export([classic/1, with_guard/1]).
 
 classic(0) -> 1;
 classic(N)  -> N * classic(N-1).
-    
+
+
+with_guard(N) when N > 0 -> 
+    N * with_guard(N-1);
+with_guard(0) -> 1.
