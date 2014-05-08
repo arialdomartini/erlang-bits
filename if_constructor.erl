4642c5691ff1a762d1f6aba3e4148d1d31c3fe48
Arialdo Martini <arialdomartini@gmail.com>
2014-05-08 08:35:59 +0200

    The use of a catch-all guard in the if constructor

--- if_constructor.erl
+++ if_constructor.erl
@@ -1,9 +1,16 @@
 -module(if_constructor).
--export([evaluate/1]).
+-export([evaluate/1, with_catch_all_guard/1]).
 
 evaluate(Number) ->
     if
-        Number < 0 -> negative;
-        Number > 0 -> positive;
+        Number < 0  -> negative;
+        Number > 0  -> positive;
         Number == 0 -> zero
+    end.
+
+with_catch_all_guard(Number) ->
+    if
+        Number > 100    -> big;
+        Number < 100   -> small;
+        true            -> all_the_rest
     end.
