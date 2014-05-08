8a9e7ff48967da66b20c6c0fec33afc7569c6c69
Arialdo Martini <arialdomartini@gmail.com>
2014-05-08 08:38:34 +0200

    odd or even with if

--- if_constructor.erl
+++ if_constructor.erl
@@ -1,5 +1,5 @@
 -module(if_constructor).
--export([evaluate/1, with_catch_all_guard/1]).
+-export([evaluate/1, with_catch_all_guard/1, odd_or_even_with_if/1]).
 
 evaluate(Number) ->
     if
@@ -14,3 +14,10 @@ with_catch_all_guard(Number) ->
         Number < 100   -> small;
         true            -> all_the_rest
     end.
+
+odd_or_even_with_if(N) ->
+    if 
+        N rem 2 == 1     -> odd;
+        N rem 2 == 0     -> even
+    end.
+    
