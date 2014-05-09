f4960e637891403b3c87699fc487664fb737c8d9
Arialdo Martini <arialdomartini@gmail.com>
2014-05-09 08:45:50 +0200

    Playing with io:format

--- io_sample.erl
+++ io_sample.erl
@@ -1,5 +1,13 @@
 -module(io_sample).
--export([read_line_from_standard_input/0]).
+-export([read_line_from_standard_input/0, write/0, format/0]).
 
 read_line_from_standard_input() ->
     io:get_line("what's your name, pal? :").
+
+write() ->
+    io:write("Hello, world!").
+
+format() ->
+    List = [5, 6, 1, 22],
+    Sum = lists:sum(List),
+    io:format("The sum of the list ~w is ~w~n", [List, Sum]).
