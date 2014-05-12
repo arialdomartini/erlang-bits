f17f2b6a4049a4397d49a47a958357ef3f17ba4c
Arialdo Martini <arialdomartini@gmail.com>
2014-05-12 08:42:56 +0200

    A recursive function that checks if an item exists in a given list

--- recursion.erl
+++ recursion.erl
@@ -1,5 +1,5 @@
 -module(recursion).
--export([add_1/1, average/1, filter_even/1]).
+-export([add_1/1, average/1, filter_even/1, exists/2]).
 
 add_1([]) -> [];
 add_1([Head | Tail]) -> [Head +1 | add_1(Tail)].
@@ -16,4 +16,8 @@ average(List) ->
 filter_even([]) -> [];
 filter_even([H|T]) when H rem 2  == 0 -> [H|filter_even(T)];
 filter_even([_|T]) -> filter_even(T).
+
+exists( _, [] )        -> false;
+exists( E, [E | _] )    -> true;
+exists( E, [_ | T] )    -> exists(E, T).
 
