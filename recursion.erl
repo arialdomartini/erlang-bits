d519aa3f3024995cad9005897147d4668459933e
Arialdo Martini <arialdomartini@gmail.com>
2014-05-12 08:38:10 +0200

    Filtering even items in a list with tail recursion)

--- recursion.erl
+++ recursion.erl
@@ -1,5 +1,5 @@
 -module(recursion).
--export([add_1/1, average/1]).
+-export([add_1/1, average/1, filter_even/1]).
 
 add_1([]) -> [];
 add_1([Head | Tail]) -> [Head +1 | add_1(Tail)].
@@ -12,3 +12,8 @@ len( [ _|T ] ) -> 1 + len(T).
 
 average(List) ->
     sum(List) / len(List).
+
+filter_even([]) -> [];
+filter_even([H|T]) when H rem 2  == 0 -> [H|filter_even(T)];
+filter_even([H|T]) when H rem 2  == 1 -> filter_even(T).
+
