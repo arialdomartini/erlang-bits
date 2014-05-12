1754935587d8988cf818e49fa3c0a23da501446f
Arialdo Martini <arialdomartini@gmail.com>
2014-05-12 08:28:58 +0200

    Average calculation with tail recursion

--- recursion.erl
+++ recursion.erl
@@ -1,6 +1,14 @@
 -module(recursion).
--export([add_1/1]).
+-export([add_1/1, average/1]).
 
 add_1([]) -> [];
 add_1([Head | Tail]) -> [Head +1 | add_1(Tail)].
-    
+
+sum([]) -> 0;
+sum( [H|T] ) -> H + sum(T).
+
+len([]) -> 0;
+len( [ _|T ] ) -> 1 + len(T).
+
+average(List) ->
+    sum(List) / len(List).
