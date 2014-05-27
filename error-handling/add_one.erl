dbc24ec279cc0f0ac1cb8d5e2615725c8e775c45
Arialdo Martini <arialdomartini@gmail.com>
2014-05-27 18:33:20 +0200

    Crashing this module results in a new value from self() in the shell.

---
+++ error-handling/add_one.erl
@@ -0,0 +1,20 @@
+-module(add_one).
+-export([start/0, request/1, loop/0]).
+
+start() ->
+    register(add_one, spawn_link(?MODULE, loop, [])).
+
+request(Int) ->
+    add_one ! {request, self(), Int},
+    receive
+        {result, Result} ->
+            Result
+    after 1000 -> timeout
+    end.
+
+loop() ->
+    receive
+        {request, Pid, Int} ->
+            Pid ! {result, Int + 1}
+    end,
+    loop().
