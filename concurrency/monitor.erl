692c05c7e0e4793675fa9483374c2dd89548d4fa
Arialdo Martini <arialdomartini@gmail.com>
2014-05-28 17:31:50 +0200

    Monitoring processes

---
+++ concurrency/monitor.erl
@@ -0,0 +1,39 @@
+-module(monitor).
+-export([start/0, do/0, crash/0]).
+-export([init/0]).
+
+start() ->
+    Pid = spawn(?MODULE, init, []),
+    register(other, Pid),
+    {ok, Pid, other}.
+
+do() ->
+    other ! {request, self(), do},
+    receive
+        {reply, Reply} -> {ok, Reply};
+        _ -> other
+    end.
+
+crash() ->
+    erlang:monitor(process, other),
+    other ! {request, self(), crash},
+    receive
+        {'DOWN', _Reference, process, Pid, Reason} -> 
+            io:format("Oh, ~w crashed because ~w", [Pid, Reason]);
+        Other -> Other
+    end.
+
+
+init() ->
+    loop().
+
+loop() ->
+    receive
+        {request, Pid, do} ->
+            Pid ! {reply, ok},
+            loop();
+        {request, _Pid, crash} ->
+            1/0
+    end.
+
+
