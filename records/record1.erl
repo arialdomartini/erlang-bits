3533839d0d62ba32e2197c2afdc488931f92454c
Arialdo Martini <arialdomartini@gmail.com>
2014-06-06 09:01:40 +0200

    Same tuple as a record, hence defined only once

---
+++ records/record1.erl
@@ -0,0 +1,19 @@
+-module(record1).
+-export([test1/0, test2/0]).
+-record(person, {name, age, phone}).
+
+test1() ->
+    showPerson(joe()).
+
+test2() ->
+    showPerson(birthday(joe())).
+
+joe() ->
+    #person{name = "Joe", age = 21, phone ="0573-278271"}.
+
+birthday(Person) ->
+    Person#person{age = Person#person.age + 1}.
+
+showPerson(Person) ->
+    io:format("name: ~p, age: ~p, phone: ~p", [Person#person.name, Person#person.age, Person#person.phone]).
+
