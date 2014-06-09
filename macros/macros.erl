35bf31d40c65489eec7aae4b475c0417497c0662
Arialdo Martini <arialdomartini@gmail.com>
2014-06-09 18:25:38 +0200

    A simple macro defined in a module

---
+++ macros/macros.erl
@@ -0,0 +1,38 @@
+-module(macros).
+-export([start/0, send/1]).
+-export([init/0]).
+
+-define(TIMEOUT, 2500).
+
+
+start() ->
+    process_flag(trap_exit, true),
+    register(macros, spawn(?MODULE, init, [])),
+    link(whereis(macros)),
+    io:format("Process started with PID~p~nYou have ~p seconds to run send(N)~n", [whereis(macros), ?TIMEOUT/1000]),
+    ok.
+
+send(Number) ->
+    macros ! {request, self(), Number},
+    receive
+        {reply, Reply} ->
+            Reply;
+        {'EXIT', _Pid, Reason} ->  
+            {error, Reason}
+    end.
+
+init() ->
+    loop().
+
+loop() ->
+    receive
+        {request, Pid, N} ->
+            reply(Pid, {ok, N+1}),
+            loop()
+    after ?TIMEOUT ->
+            io:format("Timeout. I'm quitting"),
+            {error, timeout}
+    end.
+
+reply(Pid, Message) ->
+    Pid ! {reply, Message}.
