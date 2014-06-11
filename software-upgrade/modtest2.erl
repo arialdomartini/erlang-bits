3b09e4745200b70d36dc29db8f21f2906e2b47d0
Arialdo Martini <arialdomartini@gmail.com>
2014-06-11 19:18:24 +0200

    Depending on calling the function with a fully qualidied name or not the old version is used

---
+++ software-upgrade/modtest2.erl
@@ -0,0 +1,18 @@
+-module(modtest2).
+-export([main/0, loop/0, a/1, do/1]).
+
+main() ->
+    register(foo, spawn(modtest2, loop, [])).
+
+loop() ->
+    receive
+        {Sender, N} ->
+            Sender ! modtest2:a(N)
+    end,
+    loop().
+
+do(M) ->
+    foo ! {self(), M},
+    receive Reply -> Reply end.
+
+a(N) -> N + 1.
