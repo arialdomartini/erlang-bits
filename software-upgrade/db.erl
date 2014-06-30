09ba9b52ab7d970f59bf6a25d3757e828e4cbfa9
Arialdo Martini <arialdomartini@gmail.com>
2014-06-30 08:45:25 +0200

    db module using gb_trees: hot upgraded

--- software-upgrade/db.erl
+++ software-upgrade/db.erl
@@ -1,26 +1,37 @@
 -module(db).
--export([new/0, write/3, read/2, delete/2, destroy/1]).
+-export([new/0, write/3, read/2, delete/2, destroy/1, convert/2]).
 -export([test/0, info/0]).
--vsn(1.2).
+-vsn(1.3).
 
 new() ->
-    dict:new().
+    gb_trees:empty().
 
 write(Key, Data, Db) ->
-    dict:store(Key, Data, Db).
+    gb_trees:insert(Key, Data, Db).
 
 read(Key, Db) ->
-    case dict:find(Key, Db) of
-        error -> {error, instance};
-        {ok, Data} -> {ok, Data}
+    case gb_trees:lookup(Key, Db) of
+        none -> {error, instance};
+        {valye, Data} -> {ok, Data}
     end.
 
 delete(Key, Db) ->
-    dict:erase(Key, Db).
+    gb_trees:delete(Key, Db).
 
 destroy(_Db) ->
     {ok, "Not yet implemented"}.
 
+convert(dict, Dict) ->
+    dict(dict:fetch_keys(Dict), Dict, new());
+convert(_, Data) ->
+    Data.
+
+dict([Key|Tail], Dict, GbTree) ->
+    Data = dict:fetch(Key, Dict),
+    NewGbTree = gb_trees:insert(Key, Data, GbTree),
+    dict(Tail, Dict, NewGbTree);
+dict([], _, GbTree) ->
+    GbTree.
 
 test() ->
         io:format("call this function as Db2 = db:test().~n"),
