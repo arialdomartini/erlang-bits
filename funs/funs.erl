07ab3d69cd1fd46bf742a98d86c42161b61fe0d8
Arialdo Martini <arialdomartini@gmail.com>
2014-06-30 18:14:46 +0200

    Defining and directly running an anonymous function

--- funs/funs.erl
+++ funs/funs.erl
@@ -1,7 +1,10 @@
 -module(funs).
--export([run/0]).
+-export([run/0, direct/0]).
 
 run() ->
     Bump = fun(Int) ->
                    Int + 1 end,
     Bump(100).
+
+direct() ->
+    (fun(Int) -> Int + 1 end)(4).
