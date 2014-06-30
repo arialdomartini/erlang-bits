42167ab5084f48ebbe1884a60f98e6ff46925d6e
Arialdo Martini <arialdomartini@gmail.com>
2014-06-30 18:37:13 +0200

    An ordinary function that filters even numbers

--- funs/funs.erl
+++ funs/funs.erl
@@ -1,5 +1,5 @@
 -module(funs).
--export([run/0, direct/0, double_all/1, plus1_all/1]).
+-export([run/0, direct/0, double_all/1, plus1_all/1, evens/1]).
 
 run() ->
     Bump = fun(Int) ->
@@ -12,8 +12,12 @@ direct() ->
 map(_,[]) -> [];
 map(F, [X|Xs]) -> [ F(X) | map(F, Xs) ].
 
-double_all(List) -> map( fun(X) -> X*2 end, List).
+double_all(List) -> map( double, List).
 
 plus1_all(List) -> map (fun(X) -> X+1 end ,List).
 
 
+evens([]) -> [];
+evens([X | Xs]) when X rem 2 == 0 -> [X | evens(Xs) ];
+evens([X | Xs]) when X rem 2 =/= 0 -> evens(Xs).
+    
