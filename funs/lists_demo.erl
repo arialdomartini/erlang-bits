6b855addcd2b1dab45915a049287a66057b7edaf
Arialdo Martini <arialdomartini@gmail.com>
2014-07-07 08:49:46 +0200

    Another example of built-in lists funs: any

--- funs/lists_demo.erl
+++ funs/lists_demo.erl
@@ -1,12 +1,20 @@
 -module(lists_demo).
--export([run/0]).
+-export([all/0, any/0]).
 -export([is_greater_than/2]).
+-export([allows/1]).
 
 is_greater_than(X, Y) ->
     X > Y.
 
-run() ->
+all() ->
     X = [1, 2, 3, 4, 5],
     Y = [3, 4, 5, 6, 7],
     If_greater_than_2 = fun(R) -> lists_demo:is_greater_than(R, 2) end,
     {X, lists:all(If_greater_than_2, X), Y, lists:all(If_greater_than_2, Y)}.
+
+allows(X) ->
+    X == yes.
+
+any() ->
+    X = [no, nada, niet, yes],
+    {X, lists:any(fun lists_demo:allows/1, X)}.
