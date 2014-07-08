039433be5cc71337f15e2022b4a0671ff4f5193b
Arialdo Martini <arialdomartini@gmail.com>
2014-07-08 08:39:52 +0200

    The bif lists:filter implemented with a list comprehension and a fun

--- funs/my_lists.erl
+++ funs/my_lists.erl
@@ -1,6 +1,8 @@
 -module(my_lists).
 -export([map/0]).
 -export([map/2, double/1]).
+-export([filter/0]).
+-export([filter/2, is_even/1]).
 
 double(X) ->
     2 * X.
@@ -10,3 +12,12 @@ map(Function, List) ->
 
 map() ->
     map(fun my_lists:double/1, [1,2,3,4]).
+
+is_even(X) ->
+    X rem 2 == 0.
+
+filter(Function, List) ->
+    [Item || Item <- List, Function(Item) == true].
+
+filter() ->
+    filter(fun my_lists:is_even/1, [1,2,3,4,5,6]).
