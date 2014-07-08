4a027c605874fb8db80e860333a6ad2329299722
Arialdo Martini <arialdomartini@gmail.com>
2014-07-08 08:57:07 +0200

    binary_split() can be used to cut a binary in two parts

--- binary/bin.erl
+++ binary/bin.erl
@@ -5,4 +5,5 @@ run() ->
     Binary = term_to_binary({foo, bar, 12, "OK"}),
     io:format("~w~n", [Binary]),
     Orig = binary_to_term(Binary),
-    io:format("~w~n", [Orig]).
+    io:format("~w~n", [Orig]),
+    io:format("~w~n", [split_binary(Binary, 1)]).
