df16ce231dbcf4115a7529ff6868503ca3730e8b
Arialdo Martini <arialdomartini@gmail.com>
2014-07-14 08:47:48 +0200

    Inserting 2 values with the same key in a set overwrites the old values; a bag stores both

--- erlang-term-storage/ets_tables.erl
+++ erlang-term-storage/ets_tables.erl
@@ -1,5 +1,5 @@
 -module(ets_tables).
--export([info/0, insert_lookup/0]).
+-export([info/0, set_insert_lookup/0, bag_insert_lookup/0]).
 
 
 info() ->
@@ -8,8 +8,16 @@ info() ->
     ets:delete(Set),
     Info.
 
-insert_lookup() ->
+set_insert_lookup() ->
     Set = ets:new(my_set, [named_table]),
     ets:insert(Set, {key, value}),
+    ets:insert(Set, {key, bar}),
     Value = ets:lookup(Set, key),
-    io:format("At key ~w the set stored the value ~w~n", [key, Value]).
+    io:format("At key ~w the set stored only the last value ~w~n", [key, Value]).
+
+bag_insert_lookup() ->
+    Bag = ets:new(my_bag, [bag]),
+    ets:insert(Bag, {foo, bar}),
+    ets:insert(Bag, {foo, baz}),
+    Values = ets:lookup(Bag, foo),
+    io:format("At key ~w the bag stored both the balues: ~w", [foo, Values]). 
