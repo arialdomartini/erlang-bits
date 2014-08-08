05470a9ca950878abb5df70a20778109aedd5a15
Arialdo Martini <arialdomartini@gmail.com>
2014-08-08 08:42:27 +0200

    Can persist a new user to Mnesia

--- mymnesia/usr.erl
+++ mymnesia/usr.erl
@@ -1,6 +1,6 @@
 -module(usr).
 -export([create_tables/0, ensure_loaded/0, write_record/0]). 
-
+-export([add_usr/3]).
 -include("usr.hrl").
 
 
@@ -18,3 +18,10 @@ ensure_loaded() ->
 write_record() ->
     Rec = #usr{msisdn=700000003, id=3, status=enabled, plan=prepay, services=[data,sms,lbs]},
     mnesia:transaction(fun() -> mnesia:write(Rec) end).
+
+
+add_usr(PhoneNo, CustId, Plan) when Plan==prepay; Plan==postpay -> 
+    Rec = #usr{msisdn = PhoneNo, id = CustId, plan = Plan},
+    Fun = fun() -> mnesia:write(Rec) end,
+    {atomic, Res} = mnesia:transaction(Fun), 
+    Res. 
